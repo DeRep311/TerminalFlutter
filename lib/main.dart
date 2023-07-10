a879ba2ea2c1a5967cdb246cb02119e1386d8ee0
DeRep311 <endcusatt26@gmail.com>
2023-07-10 17:40:07 -0300

    routing system new

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:my_app/src/Widgets/Cards.dart';
-import 'package:my_app/src/Widgets/GridNavigation.dart';
-import 'package:my_app/src/Widgets/Layout.dart';
-import 'package:my_app/src/Widgets/OptionBox.dart';
-import 'package:my_app/src/Widgets/header.dart';
+import 'package:my_app/src/Screens/Home.dart';
 import 'package:window_manager/window_manager.dart';
 
-void main() async {
-  runApp(const MyApp());
+void windowsManager() async {
   WidgetsFlutterBinding.ensureInitialized();
 // Initialize the WindowManager instance.
   await windowManager.ensureInitialized();
@@ -28,59 +23,24 @@ void main() async {
   });
 }
 
+void main() async {
+  runApp(const MyApp());
+  windowsManager();
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        home: Scaffold(
-          body: Container(
-            color: const Color.fromRGBO(255, 255, 255, 1),
-            child: Layout(
-              children: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: const [
-                  //Header main screen
-                  Header(),
-                  //Container for grid and weather
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      //options grid
-                      GridNavigation(childrens: [
-                        OptionBox(
-                            title: 'opcion 1',
-                            imagePath:
-                                'lib/src/Assets/Icons/graduation-hat-02.png'),
-                        OptionBox(
-                            title: 'opcion',
-                            imagePath:
-                                'lib/src/Assets/Icons/graduation-hat-02.png'),
-                        OptionBox(
-                            title: 'opcion 2',
-                            imagePath:
-                                'lib/src/Assets/Icons/graduation-hat-02.png'),
-                        OptionBox(
-                            title: 'opcion',
-                            imagePath:
-                                'lib/src/Assets/Icons/graduation-hat-02.png'),
-                      ]),
-                      //wheather and time
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [CardTime(), CardTime()],
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
-        )
-        );
+      title: 'Flutter Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+        
+        
+        },
+    );
   }
 }
---
+++ lib/src/Screens/Home.dart
@@ -0,0 +1,64 @@
+//need widget return scaffold
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import '../Widgets/Cards.dart';
+import '../Widgets/GridNavigation.dart';
+import '../Widgets/Layout.dart';
+import '../Widgets/OptionBox.dart';
+import '../Widgets/header.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: const Color.fromRGBO(255, 255, 255, 1),
+        child: Layout(
+          children: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: const [
+              //Header main screen
+              Header(),
+              //Container for grid and weather
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //options grid
+                  GridNavigation(childrens: [
+                    OptionBox(
+                        title: 'opcion 1',
+                        imagePath:
+                            'lib/src/Assets/Icons/graduation-hat-02.png'),
+                    OptionBox(
+                        title: 'opcion',
+                        imagePath:
+                            'lib/src/Assets/Icons/graduation-hat-02.png'),
+                    OptionBox(
+                        title: 'opcion 2',
+                        imagePath:
+                            'lib/src/Assets/Icons/graduation-hat-02.png'),
+                    OptionBox(
+                        title: 'opcion',
+                        imagePath:
+                            'lib/src/Assets/Icons/graduation-hat-02.png'),
+                  ]),
+                  //wheather and time
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [CardTime(), CardTime()],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
